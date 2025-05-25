255a9dc75a27d395e890d884f78e8be565d5aa47
Max-Kinyanjui <maxkinyanjuiii@gmail.com>
2025-05-25 19:02:40 +0300

    Convert the first arguement into an integer and check the validity the crreate a function and do the sum

---
+++ 9-add.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const a = parseInt(args[0]);
+const b = parseInt(args[1]);
+
+function add(a, b) {
+  return a + b;
+}
+
+if (isNaN(a) || isNaN(b)) {
+  console.log("Missing integers");
+} else {
+  console.log(add(a, b));
+}
