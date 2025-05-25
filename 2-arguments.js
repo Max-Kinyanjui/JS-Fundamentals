0c54c4fdc584a6ef42c4202f47e70075bb12dd17
Max-Kinyanjui <maxkinyanjuiii@gmail.com>
2025-05-25 18:25:24 +0300

    Add the if else statements

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,6 +1,16 @@
-const { argv } = require('node:process');
+// const { argv } = require('node:process');
 
-// print process.argv
-argv.forEach((val, index) => {
-  console.log(`${index}: ${val}`);
-});
+// // print process.argv
+// argv.forEach((val, index) => {
+//   console.log(`${index}: ${val}`);
+// });
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+    console.log("No argument");
+} else if (args.length === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
