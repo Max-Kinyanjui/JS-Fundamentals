8e9b51649a8b8e7d28d766c1cb48e587c8a427cf
Max-Kinyanjui <maxkinyanjuiii@gmail.com>
2025-11-23 22:14:52 +0300

    ADD the file

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,13 +1,12 @@
-#!/usr/bin/node
 function factorial(n) {
-	if (n <= 1) return 1;
-	return n * factorial(n - 1);
+  if (n <= 1) return 1;
+  return n * factorial(n - 1);
 }
 
 const arg = process.argv[2];
 const n = parseInt(arg, 10);
 if (isNaN(n)) {
-	console.log(1);
+  console.log(1);
 } else {
-	console.log(factorial(n));
+  console.log(factorial(n));
 }
