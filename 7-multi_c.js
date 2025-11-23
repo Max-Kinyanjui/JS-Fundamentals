4eb4f10c35c4f7fcc5d0242bfa83b28a654db8e0
Max-Kinyanjui <maxkinyanjuiii@gmail.com>
2025-11-23 21:50:16 +0300

    Loop through and print the string x times

---
+++ 7-multi_c.js
@@ -0,0 +1,14 @@
+const arg = process.argv[2];
+const n = parseInt(arg, 10);
+if (isNaN(n)) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  let out = "";
+  while (i < n) {
+    out += "C is fun";
+    if (i !== n - 1) out += "\n";
+    i++;
+  }
+  console.log(out);
+}
